227f4993b13816914d7116b573d55fed9967ab8b
vijidy <viji.v@datayaan.com>
2024-04-02 18:22:06 +0530

    eda and model

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
-import logging 
-import logger
+
+from src.logger import logging
 
 def error_message_details(error, error_detail:sys):
     _,_,exc_tb =error_detail.exc_info()
@@ -21,9 +21,9 @@ class customException(Exception):
     
 
 
-if __name__=="__main__":
-    try :
-        a=1/0
-    except Exception as e:
-        logging.info("Divided by Zero gives Error")
-        raise customException(e,sys)
+# if __name__=="__main__":
+#     try :
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divided by Zero gives Error")
+#         raise customException(e,sys)
