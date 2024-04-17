c5b05906852d9570c100213fb0b6713e02e34364
vijidy <viji.v@datayaan.com>
2024-04-17 17:50:21 +0530

    'docker'

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
-
 from src.pipeline.predict_pipeline import CustomData,predictpipeline
 
-
 application= Flask(__name__)
-
 
 app = application
 
@@ -33,19 +30,20 @@ def predict_datapoint():
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('writing_score')),
             writing_score=float(request.form.get('reading_score'))
-
         )
+
         pred_df=data.get_data_as_data_frame()
+
         print(pred_df)
         print("Before Prediction")
 
         predict_pipeline=predictpipeline()
         print("Mid Prediction")
+
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
+        
         return render_template('home.html',results=results[0])
     
-
 if __name__ == "__main__":
-
     app.run(host='0.0.0.0', port=8080,debug=True)
